5d71c239190cfc17a8674e97f0d42e9c1cdd532b
maykicn <maykic@gmail.com>
2020-12-04 15:47:42 +0200

    Adding new line of comment locally , not push yet

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Sill really my Coworker added awesome hello");  //We are simulating the situation my coworker made this change
-      
+        //add local one  ore line
 
     }
 }
