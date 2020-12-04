1d4e8733a1105009077822c908aee00727ee668c
maykicn <maykic@gmail.com>
2020-12-04 15:43:41 +0200

    Added my own hello

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Sill reallyawesome hello");
-        //add one more line
+        System.out.println("This is my own hello"); //this is my local hello,and commiting
+
 
     }
 }
